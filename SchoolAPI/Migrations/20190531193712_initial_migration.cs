e3e31ea0dc37a3f175260d37aacfb943d0379620
Medhat Elmasry <medhat@elmasry.ca>
2019-05-31 17:31:04 -0700

    First Commit

---
+++ SchoolAPI/Migrations/20190531193712_initial_migration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolAPI.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    School = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "FirstName", "LastName", "School" },
+                values: new object[,]
+                {
+                    { "8734ce0b-4337-487a-bd86-102130d47f8d", "Jane", "Smith", "Medicine" },
+                    { "db356c3a-d745-4f65-9aac-234f706859c2", "John", "Fisher", "Engineering" },
+                    { "7721c620-a82b-4204-80af-6c7636efc81e", "Pamela", "Baker", "Food Science" },
+                    { "9a85f51f-160e-4003-9de9-b01354e180a3", "Peter", "Taylor", "Mining" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
